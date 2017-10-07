5446ddfb19239bba1c844f7fbd3140ed7b482e42
José Miguel <josemiguel@intelligenia.com>
2017-10-07 19:28:58 +0200

    settings

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+Version = 0.1
+setup ( name='django-rest_mockup',
+        version = Version,
+        install_requires='jsonfield',
+        description = "TODO",
+        long_description = "TODO",
+        author = "Jose Miguel Lopez",
+        author_email = "josmilope@gmail.com",
+        url = "https://github.com/josemlp91/django_rest_mockup",
+        packages = ['django-rest_mockup',],
+        license = 'MIT',
+        platforms = 'Posix; MacOS X;',
+        classifiers = [
+            
+        ],
+     )
